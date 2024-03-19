ec8a9bcfd212fa8874f07a3be0dc2693235dfdf2
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-19 14:21:39 -0300

    final version

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
-// obtendo o jogo clicado
-function alterarStatus (id) { // o parâmetro são os Ids
-    let gameClicado = document.getElementById (`game-${id}`); // concatenar e buscar o id da variavel "game" no HTML
-    let imagem = gameClicado.querySelector (".dashboard__item__img"); // para alterar a imagem do jogo (alugado/devolver)
-    let botao = gameClicado.querySelector (".dashboard__item__button"); // o "." dentro significa que é uma classe // altera o botão 
+function alterarStatus (id) { 
+    let gameClicado = document.getElementById (`game-${id}`); 
+    let imagem = gameClicado.querySelector (".dashboard__item__img"); 
+    let botao = gameClicado.querySelector (".dashboard__item__button"); 
 
-    if (imagem.classList.contains("dashboard__item__img--rented")) { // se o jogo estiver alugado/devolvido // contains = se o elemento contem uma determinada classe
-        imagem.classList.remove ("dashboard__item__img--rented"); // para remover o alugado e ser devolvido 
+    if (imagem.classList.contains("dashboard__item__img--rented")) { 
+        imagem.classList.remove ("dashboard__item__img--rented"); 
         botao.classList.remove ("dashboard__item__button--return");
-        botao.textContent = "Alugar" // textContent = muda o texto do <a> // para mudar o texto do botao para alugar ou devolver
-    } else {                                                    // fazendo o contrário
-        imagem.classList.add ("dashboard__item__img--rented");   // adicionando 
+        botao.textContent = "Alugar" 
+    } else {                                                 
+        imagem.classList.add ("dashboard__item__img--rented");   
         botao.textContent = "Devolver";
         botao.classList.add ("dashboard__item__button--return");
     }
